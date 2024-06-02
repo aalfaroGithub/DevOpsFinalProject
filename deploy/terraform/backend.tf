f4f187d85c8c2ce89e431263d6d43ed7d36383a4
andres <aalfaro@perhead.com>
2024-06-02 12:27:25 -0600

    Removing s3

--- deploy/terraform/backend.tf
+++ deploy/terraform/backend.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   // Adding a s3 bucket to store the terraform state file
-  backend "s3" {
-    bucket = "terraform-state-bucket-123456789"
-    key    = "terraform.tfstate"
-    region = "us-west-2"
-  }
+  # backend "s3" {
+  #   bucket = "terraform-state-bucket-123456789"
+  #   key    = "terraform.tfstate"
+  #   region = "us-west-2"
+  # }
 }
